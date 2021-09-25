b49227c99aa3c68ad0fa8ccf9471d3fb6f72fa4d
syedalihussain100 <smuheminali@gmail.com>
2021-09-24 23:51:03 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let phonenumber = document.getElementById('phonenumber');
+
+
+email.addEventListener('blur', () => {
+
+})
+
+password.addEventListener('blur', () => {
+
+})
+
+phonenumber.addEventListener('blur', () => {
+
+})
